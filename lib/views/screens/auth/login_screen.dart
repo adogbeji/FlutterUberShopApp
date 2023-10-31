59c2c6b9da916559e2a9f83cb8e002853045b63d
adogbeji <baka_1993@yahoo.co.uk>
2023-10-31 20:54:16 +0000

    Add TextFormField to login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -5,9 +5,11 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: Column(
+        children: [
+          TextFormField()
+        ],
       ),
     );
   }
