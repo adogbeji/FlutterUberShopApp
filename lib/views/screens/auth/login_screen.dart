953d575ebb23d7ff53b5d42b0612aed72d3b4e89
adogbeji <baka_1993@yahoo.co.uk>
2023-11-24 20:54:05 +0000

    Add padding & create more space between input & text in login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -6,53 +6,59 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Login',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 4,
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 4,
+              ),
             ),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Email Address',
+            const SizedBox(
+              height: 25,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Password',
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email Address',
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            height: 50,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(10),
+            const SizedBox(
+              height: 25,
             ),
-            child: const Center(
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  letterSpacing: 4,
-                  fontWeight: FontWeight.bold,
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    letterSpacing: 4,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
