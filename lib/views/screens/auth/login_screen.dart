de8504a70fca90531c4fd74d62111ab5b5975d45
adogbeji <baka_1993@yahoo.co.uk>
2024-02-08 20:40:04 +0000

    Add login & register screens

---
+++ lib/views/screens/auth/login_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import './register_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 4,
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email Address',
+                hintText: 'Enter Email Address',
+                prefixIcon: Icon(Icons.email, color: Colors.pink,),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter Password',
+                prefixIcon: Icon(Icons.lock, color: Colors.pink,),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            InkWell(
+              onTap: () {
+                print('Logged In!');
+              },
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width - 40,
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      letterSpacing: 4,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // InkWell(
+            //   onTap: () {},
+            //   child: const Text(
+            //     'Need An Account?',
+            //   ),
+            // ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return const RegisterScreen();
+                  }),
+                );
+              },
+              child: const Text('Need An Account?'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/auth/register_screen.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+import './login_screen.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Register Account',
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 4,
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+          
+          // EMAIL INPUT FIELD
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Email Address',
+              hintText: 'Enter Email Address',
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.pink,
+              ),
+              // border: OutlineInputBorder(),
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+          
+          // NAME INPUT FIELD
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Full Name',
+              hintText: 'Enter Full Name',
+              prefixIcon: Icon(
+                Icons.person,
+                color: Colors.pink,
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+          
+          // PASSWORD INPUT FIELD
+          TextFormField(
+            decoration: const InputDecoration(
+              labelText: 'Password',
+              hintText: 'Enter Password',
+              prefixIcon: Icon(
+                Icons.lock,
+                color: Colors.pink,
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          // REGISTER BUTTON
+          Container(
+            height: 50,
+            width: MediaQuery.of(context).size.width - 40,
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Center(
+              child: Text(
+                'Register',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  letterSpacing: 4,
+                ),
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 20,
+          ),
+
+          TextButton(
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const LoginScreen();
+              }),);
+            },
+            child: const Text(
+              'Already have an account?',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
