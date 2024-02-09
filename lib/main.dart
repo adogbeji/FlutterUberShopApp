fcc9e4a927cbc5c2dc6a9c710514d62872cfbfcb
adogbeji <baka_1993@yahoo.co.uk>
2024-02-09 21:42:35 +0000

    Copmlete Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import './views/screens/auth/login_screen.dart';
 // import './views/screens/auth/register_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Ensures all widgets have been initialised
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyCyrhSECtPtFLwQ2FpO7NH6sqamscfIWRg',
+              appId: '1:598483068590:android:2aab2445e54e9331a92f3f',
+              messagingSenderId: '598483068590',
+              projectId: 'uber-shop-app-2767c',
+              storageBucket: 'gs://uber-shop-app-2767c.appspot.com'))
+      : await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
