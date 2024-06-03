d9cbd07ec5afd05a1d83cd86162992c87b0a8674
adogbeji <baka_1993@yahoo.co.uk>
2024-06-03 10:40:58 +0100

    Wrap login button with InkWell & add registration prompt button in login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -6,57 +6,78 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Log In',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 4,
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Log In',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 4,
+              ),
             ),
-          ),
 
-          // EMAIL INPUT FIELD
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Email',
+            const SizedBox(
+              height: 25,
             ),
-          ),
 
-          const SizedBox(height: 20,),
-
-          // PASSWORD INPUT FIELD
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Password',
+            // EMAIL INPUT FIELD
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
             ),
-          ),
 
-          const SizedBox(height: 20,),
-
-          // LOGIN BUTTON
-          Container(
-            height: 50,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(10),
+            const SizedBox(
+              height: 20,
             ),
-            child: const Center(
-              child: Text(
-                'Log In',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 4,
+
+            // PASSWORD INPUT FIELD
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            // LOGIN BUTTON
+            InkWell(
+              onTap: () {
+                print('Pressed!');
+              },
+              child: Container(
+                height: 50,
+                width: MediaQuery.of(context).size.width - 40,
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Log In',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 4,
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+
+            TextButton(
+              onPressed: () {},
+              child: const Text('Need An Account?'),
+            )
+          ],
+        ),
       ),
     );
   }
