98e3cfb98ef73ff8f8d53b6ef517b240348cedb6
adogbeji <baka_1993@yahoo.co.uk>
2024-06-08 10:31:50 +0100

    Initialise Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:uber_shop_app/secrets/secret_keys.dart';
 
 import 'package:uber_shop_app/views/screens/auth/login_screen.dart';
 import 'package:uber_shop_app/views/screens/auth/register_screen.dart';
 
-void main() {
+// SECRET KEYS
+final secrets = SecretKeys();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensures all Flutter widgets have been successfully initialised
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+              apiKey: secrets.apiKey,
+              appId: secrets.appId,
+              messagingSenderId: secrets.messagingSenderId,
+              projectId: secrets.projectId,
+              storageBucket: secrets.storageBucket),
+          )
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
