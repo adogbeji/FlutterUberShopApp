1e56ac331825de0718ea832a84bf0dcb64756c10
adogbeji <baka_1993@yahoo.co.uk>
2024-06-10 10:36:51 +0100

    Create form key & bind it to form

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -90,7 +90,7 @@ class LoginScreen extends StatelessWidget {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return const RegisterScreen();
+                      return RegisterScreen();
                     },
                   ),
                 );
--- lib/views/screens/auth/register_screen.dart
+++ lib/views/screens/auth/register_screen.dart
@@ -3,135 +3,139 @@ import 'package:flutter/material.dart';
 import 'package:uber_shop_app/views/screens/auth/login_screen.dart';
 
 class RegisterScreen extends StatelessWidget {
-  const RegisterScreen({super.key});
+  // const RegisterScreen({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();  // Form Key
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Register',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 4,
-            ),
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          // EMAIL INPUT FIELD
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Email Address Must Not Be Empty!';
-              } else {
-                return null;
-              }
-            },
-            decoration: const InputDecoration(
-              labelText: 'Email Address',
-              hintText: 'Enter Email Address',
-              prefixIcon: Icon(
-                Icons.email,
-                color: Colors.pink,
+      body: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Register',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 4,
               ),
             ),
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          // NAME INPUT FIELD
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Name Must Not Be Empty!';
-              } else {
-                return null;
-              }
-            },
-            decoration: const InputDecoration(
-              labelText: 'Full Name',
-              hintText: 'Enter Full Name',
-              prefixIcon: Icon(
-                Icons.person,
-                color: Colors.pink,
-              ),
+        
+            const SizedBox(
+              height: 20,
             ),
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          // PASSWORD INPUT FIELD
-          TextFormField(
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Password Must Not Be Empty!';
-              } else {
-                return null;
-              }
-            },
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              hintText: 'Enter Password',
-              prefixIcon: Icon(
-                Icons.lock,
-                color: Colors.pink,
-              ),
-            ),
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          // REGISTER BUTTON
-          Container(
-            height: 50,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: const Center(
-              child: Text(
-                'Register',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  letterSpacing: 4,
+        
+            // EMAIL INPUT FIELD
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Email Address Must Not Be Empty!';
+                } else {
+                  return null;
+                }
+              },
+              decoration: const InputDecoration(
+                labelText: 'Email Address',
+                hintText: 'Enter Email Address',
+                prefixIcon: Icon(
+                  Icons.email,
+                  color: Colors.pink,
                 ),
               ),
             ),
-          ),
-
-          const SizedBox(
-            height: 20,
-          ),
-
-          TextButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const LoginScreen();
-                  },
+        
+            const SizedBox(
+              height: 20,
+            ),
+        
+            // NAME INPUT FIELD
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Name Must Not Be Empty!';
+                } else {
+                  return null;
+                }
+              },
+              decoration: const InputDecoration(
+                labelText: 'Full Name',
+                hintText: 'Enter Full Name',
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Colors.pink,
                 ),
-              );
-            },
-            child: const Text('Already Have An Account?'),
-          ),
-        ],
+              ),
+            ),
+        
+            const SizedBox(
+              height: 20,
+            ),
+        
+            // PASSWORD INPUT FIELD
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Password Must Not Be Empty!';
+                } else {
+                  return null;
+                }
+              },
+              decoration: const InputDecoration(
+                labelText: 'Password',
+                hintText: 'Enter Password',
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Colors.pink,
+                ),
+              ),
+            ),
+        
+            const SizedBox(
+              height: 20,
+            ),
+        
+            // REGISTER BUTTON
+            Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Center(
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 4,
+                  ),
+                ),
+              ),
+            ),
+        
+            const SizedBox(
+              height: 20,
+            ),
+        
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const LoginScreen();
+                    },
+                  ),
+                );
+              },
+              child: const Text('Already Have An Account?'),
+            ),
+          ],
+        ),
       ),
     );
   }
