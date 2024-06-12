e8dba75b8bbfb2f989f1e272981b7764b847d222
adogbeji <baka_1993@yahoo.co.uk>
2024-06-12 10:19:22 +0100

    Bind form key to form & check current state of form in login screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -11,108 +11,115 @@ class LoginScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Log In',
-              style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 4,
-              ),
-            ),
-
-            const SizedBox(
-              height: 25,
-            ),
-
-            // EMAIL INPUT FIELD
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Email Address Must Not Be Empty!';
-                } else {
-                  return null;
-                }
-              },
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter Email Address',
-                prefixIcon: Icon(
-                  Icons.email,
-                  color: Colors.pink,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Log In',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 4,
                 ),
               ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            // PASSWORD INPUT FIELD
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Password Must Not Be Empty!';
-                } else {
-                  return null;
-                }
-              },
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter Password',
-                prefixIcon: Icon(
-                  Icons.lock,
-                  color: Colors.pink,
+          
+              const SizedBox(
+                height: 25,
+              ),
+          
+              // EMAIL INPUT FIELD
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Email Address Must Not Be Empty!';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Enter Email Address',
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.pink,
+                  ),
                 ),
               ),
-            ),
-
-            const SizedBox(
-              height: 20,
-            ),
-
-            // LOGIN BUTTON
-            InkWell(
-              onTap: () {
-                print('Pressed!');
-              },
-              child: Container(
-                height: 50,
-                width: MediaQuery.of(context).size.width - 40,
-                decoration: BoxDecoration(
-                  color: Colors.pink,
-                  borderRadius: BorderRadius.circular(10),
+          
+              const SizedBox(
+                height: 20,
+              ),
+          
+              // PASSWORD INPUT FIELD
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Password Must Not Be Empty!';
+                  } else {
+                    return null;
+                  }
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  hintText: 'Enter Password',
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.pink,
+                  ),
                 ),
-                child: const Center(
-                  child: Text(
-                    'Log In',
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 4,
+              ),
+          
+              const SizedBox(
+                height: 20,
+              ),
+          
+              // LOGIN BUTTON
+              InkWell(
+                onTap: () {
+                  if (_formKey.currentState!.validate()) {
+                    print('Valid!');
+                  } else {
+                    print('Not Valid!');
+                  }
+                },
+                child: Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width - 40,
+                  decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 4,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return RegisterScreen();
-                    },
-                  ),
-                );
-              },
-              child: const Text('Need An Account?'),
-            ),
-          ],
+          
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RegisterScreen();
+                      },
+                    ),
+                  );
+                },
+                child: const Text('Need An Account?'),
+              ),
+            ],
+          ),
         ),
       ),
     );
