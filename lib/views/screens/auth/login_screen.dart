2d50808e7070e1d930a5e37fb693511511395a77
adogbeji <baka_1993@yahoo.co.uk>
2024-07-22 10:25:17 +0100

    Wrap parent Column with Padding widget & create space between input fields in register screen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -6,53 +6,62 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            'Log In',
-            style: TextStyle(
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 4,
+      body: Padding(
+        padding: const EdgeInsets.all(14.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Log In',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 4,
+              ),
             ),
-          ),
 
-          // EMAIL INPUT FIELD
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Email',
+            const SizedBox(height: 20,),
+        
+            // EMAIL INPUT FIELD
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Email',
+              ),
             ),
-          ),
-
-          // PASSWORD INPUT FIELD
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Password',
+        
+            const SizedBox(height: 20,),
+        
+            // PASSWORD INPUT FIELD
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Password',
+              ),
             ),
-          ),
-
-          // LOGIN BUTTON
-          Container(
-            height: 50,
-            width: MediaQuery.of(context).size.width - 40,
-            decoration: BoxDecoration(
-              color: Colors.pink,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: const Center(
-              child: Text(
-                'Log In',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 4,
+        
+            const SizedBox(height: 20,),
+        
+            // LOGIN BUTTON
+            Container(
+              height: 50,
+              width: MediaQuery.of(context).size.width - 40,
+              decoration: BoxDecoration(
+                color: Colors.pink,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Center(
+                child: Text(
+                  'Log In',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
