21ff69b376fbfc624c17f63e0cdadbdd7d7c68bc
adogbeji <baka_1993@yahoo.co.uk>
2024-08-16 10:13:56 +0100

    Create function in AuthController class to select image from gallery or capture with camera

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,9 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AuthController {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;  // Stores cloud_firestore package
   final FirebaseAuth _auth = FirebaseAuth.instance;  // Stores firebase_auth package
+
+  // Selects image from phone gallery or captures image with phone camera
+  pickProfileImage(ImageSource source) async {
+    final ImagePicker _imagePicker = ImagePicker();  // Stores image_picker package
+
+    XFile? _file = await _imagePicker.pickImage(source: source);
+
+    if (_file != null) {
+      return await _file.readAsBytes();  // Converts picked image into readable format
+    }
+  }
 
   // Creates New User
   Future<String> createNewUser(String email, String fullName, String password) async {
