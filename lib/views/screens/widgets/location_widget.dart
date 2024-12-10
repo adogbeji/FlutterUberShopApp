e7f1afaa071636e32d65a14377b91cc5228332ba
adogbeji <baka_1993@yahoo.co.uk>
2024-12-10 20:05:28 +0000

    Add new image & TextFormField in LocationWidget

--- lib/views/screens/widgets/location_widget.dart
+++ lib/views/screens/widgets/location_widget.dart
@@ -9,7 +9,18 @@ class LocationWidget extends StatelessWidget {
       padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
       child: Row(
         children: [
-          Image.asset('assets/icons/store-1.png', width: 30,),
+          Image.asset(
+            'assets/icons/store-1.png',
+            width: 30,
+          ),
+
+          const SizedBox(width: 15,),
+
+          Image.asset(
+            'assets/icons/pickicon.png',
+            width: 30,
+          ),
+          TextFormField(),
         ],
       ),
     );
